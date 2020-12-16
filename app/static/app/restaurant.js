6fcf7f4f0244bf9635927d10ac1ff4b16d102041
Mark Kirichev <marki0242@gmail.com>
2020-12-16 09:01:56 +0200

    - fixed frontend issues

--- app/static/app/restaurant.js
+++ app/static/app/restaurant.js
@@ -22,6 +22,16 @@ $(function () {
     $('#cartModalStep').addClass('hidden-modal-body')
     $('#bookingModalStep').removeClass('hidden-modal-body')
   })
+  $('#confirmReservation').click(function () {
+    // Take all reservation fields and construct a json object
+    // send a POST request via $ AJAX to an endpoint for ex. /reservations
+    // the POST /reservations endpoint should validate the json input (this is extra)
+    // then the data should be stored to the database (opravqi sa moi, pravi si modeli ako trqbwa)
+    // Disable the default sumbit function of the button aka make it not a submit button
+    // Create a new modal called successfulReservation showing a success message
+    // have fun!
+  })
+
   $('#btnEndStep2').click(function () {
     $('#step2').addClass('hideMe')
     $('#step3').removeClass('hideMe')
